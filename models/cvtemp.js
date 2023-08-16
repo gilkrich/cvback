1aa5a2cd7bd0cacc06c71fa60a8a8e2e16751839
gilkrich <gildebil2@gmail.com>
2023-08-16 15:38:27 +0300

    sec

--- models/cvtemp.js
+++ models/cvtemp.js
@@ -42,7 +42,8 @@ const cvSchema = new mongoose.Schema({
     degree : {
         school : {type:String},
         type : {type:String},
-        bonus: {type:String}
+        bonus: {type:String},
+        bonus2: {type:String}
     }
 
 
